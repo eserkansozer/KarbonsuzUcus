d8073b234b2c572fe41bb67577defdde05cb61fc
Serkan Sozer <eserkansozer@hotmail.com>
2019-03-28 19:27:09 +0000

    share buttons

---
+++ src/icons.ts
@@ -0,0 +1,37 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons/faGooglePlusG';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons/faWhatsapp';
+import { faFacebookMessenger } from '@fortawesome/free-brands-svg-icons/faFacebookMessenger';
+import { faMix } from '@fortawesome/free-brands-svg-icons/faMix';
+import { faXing } from '@fortawesome/free-brands-svg-icons/faXing';
+import { faLine } from '@fortawesome/free-brands-svg-icons/faLine';
+
+import { faCommentAlt } from '@fortawesome/free-solid-svg-icons/faCommentAlt';
+import { faMinus } from '@fortawesome/free-solid-svg-icons/faMinus';
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons/faEllipsisH';
+import { faLink } from '@fortawesome/free-solid-svg-icons/faLink';
+import { faExclamation } from '@fortawesome/free-solid-svg-icons/faExclamation';
+import { faPrint } from '@fortawesome/free-solid-svg-icons/faPrint';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+
+const icons = [
+  faFacebookF, faTwitter, faLinkedinIn, faGooglePlusG,
+  faWhatsapp, faFacebookMessenger, faMix, faXing, faCommentAlt, faLine,
+  faEnvelope, faCheck, faPrint, faExclamation, faLink, faEllipsisH, faMinus];
+
+library.add(faFacebookF);
+library.add(faTwitter);
+library.add(faLinkedinIn);
+library.add(faWhatsapp);
+library.add(faEnvelope);
+library.add(faPrint);
+library.add(faLink);
+library.add(faGooglePlusG);
+library.add(faFacebookMessenger);
+library.add(faCheck);
+library.add(faEllipsisH);
