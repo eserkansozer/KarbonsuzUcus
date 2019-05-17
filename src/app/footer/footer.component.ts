fb5dd4fa2c422ccb42604be409412096f2199bda
Serkan <eserkansozer@hotmail.com>
2019-05-17 15:32:08 +0100

    EgeOrman logo

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -14,6 +14,7 @@ import { faPrint } from '@fortawesome/free-solid-svg-icons/faPrint';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  referrer: string;
   constructor() {}
 
   ngOnInit() {
@@ -24,5 +25,6 @@ export class FooterComponent implements OnInit {
     library.add(faPrint);
     library.add(faGooglePlusG);
     library.add(faFacebookMessenger);
+    this.referrer = sessionStorage.referrer;
   }
 }
