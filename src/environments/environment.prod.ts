8aac4b06cec1319a46cd2b3f7d1b4d2f94260cfe
Serkan Sozer <serkan@groovytechs.co.uk>
2019-12-20 15:53:10 +0000

    Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  donationApiServiceUrl: 'https://offset-my-carbon-api.azurewebsites.net/api'
+  donationApiServiceUrl: 'https://offset-my-carbon-api.azurewebsites.net/api/donations'
 };
